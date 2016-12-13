c5527694434e8256886fec313f4df914b3698c26
pesehr <sepehr.sabour@gmail.com>
2016-12-13 21:52:58 +0330

    inital commit

--- index.php
+++ index.php
@@ -5,26 +5,6 @@ $username = "root";
 $password = "";
 $dbname = "test";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-$sql = "CREATE TABLE MyGuests (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP
-)";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Table MyGuests created successfully";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
-
-
 $str = "Hello world!";
 echo $str;
 echo "<br>What a nice day!";
